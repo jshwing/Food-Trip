e441d83b15821336bbf612e33e7e61b725c74809
jshwing <jshwing789@naver.com>
2022-08-19 17:32:45 +0900

    8th commit

---
+++ indexscript.js
@@ -0,0 +1,10 @@
+const searchInput = document.getElementById('search');
+const searchBtn = document.getElementById('searchBtn');
+const data = document.getElementById('innerT')
+
+searchBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const val = searchInput.value;
+    console.log(val);
+    document.
+})
